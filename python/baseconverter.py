f6820858274521084a36dfdbb55bb0dfb9a9c0a2
Jack Bird <jack.bird@durham.ac.uk>
2018-10-29 19:15:17 +0000

    Created a base converter in python using someone else's code as a third part to check that the assembly produces the correct answer

---
+++ python/baseconverter.py
@@ -0,0 +1,30 @@
+def toDigits(n, b):
+    """Convert a positive number n to its digit representation in base b."""
+    digits = []
+    while n > 0:
+        digits.insert(0, n % b)
+        n  = n // b
+    return digits
+
+def fromDigits(digits, b):
+    """Compute the number given by digits in base b."""
+    n = 0
+    for d in digits:
+        n = b * n + d
+    return n
+
+def convertBase(digits, b, c):
+    """Convert the digits representation of a number from base b to base c."""
+    return toDigits(fromDigits(digits, b), c)
+
+def convert(n, a, b):
+    digits = []
+    while n > 0:
+        digits.append(n%10)
+        n = n//10
+    digits = digits[::-1]
+    digits = "".join([str(i) for i in convertBase(digits, a, b)])
+    return digits
+
+
+print(convert(684,9,3))
